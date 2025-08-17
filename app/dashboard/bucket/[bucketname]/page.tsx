d9f233aee1c0b0b8f7850a93dead87e922ddad16
Piyush Vinod Satange <piyushsatange@gmail.com>
2025-08-17 23:23:10 +0530

    added loading in folders

--- app/dashboard/bucket/[bucketname]/page.tsx
+++ app/dashboard/bucket/[bucketname]/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Loading from "@/components/loading";
 import Image from "next/image";
 import React from "react";
 import { useEffect, useState } from "react";
@@ -13,9 +14,11 @@ export default function BucketDetailPage({
   const [objects, setObjects] = useState([]);
   const [folders, setFolders] = useState([]);
   const [payload, setPayload] = useState({ folderName: "" });
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     async function getObjects() {
+      setLoading(true);
       const response = await fetch(
         `http://localhost:3000/api/bucket/${bucketname}?prefix=${payload.folderName}`
       );
@@ -44,7 +47,7 @@ export default function BucketDetailPage({
             .filter((object: any) => object.Key !== "")
         );
         // Prevent the original setObjects above
-        return;
+        setLoading(false);
       }
     }
     getObjects();
@@ -52,51 +55,60 @@ export default function BucketDetailPage({
 
   return (
     <>
-      <div className="grid grid-cols-6 gap-4">
-        {folders?.length > 0 &&
-          folders.map((folder: any) => (
-            <div
-              key={folder.Prefix} // key should be on the outermost element
-              className="flex flex-col items-center justify-center p-4 bg-gray-800 rounded-lg hover:bg-gray-700 transition"
-              onDoubleClick={() => {
-                setPayload((prev) => ({
-                  folderName: prev.folderName + folder.Prefix,
-                }));
-              }}
-            >
-              <div title={folder.Prefix}>
-                <Image
-                  src="/folder.png"
-                  alt="Folder Icon"
-                  width={150}
-                  height={150}
-                />
-                <div className="text-center">
-                  {folder.Prefix.slice(0, -1).length > 18
-                    ? `${folder.Prefix.slice(0, -1).substring(0, 18)}...`
-                    : folder.Prefix.slice(0, -1)}
+      {loading ? (
+        <Loading />
+      ) : (
+        <div className="grid grid-cols-6 gap-4">
+          {folders?.length > 0 &&
+            folders.map((folder: any) => (
+              <div
+                key={folder.Prefix} // key should be on the outermost element
+                className="flex flex-col items-center justify-center p-4 bg-gray-800 rounded-lg hover:bg-gray-700 transition"
+                onDoubleClick={() => {
+                  setPayload((prev) => ({
+                    folderName: prev.folderName + folder.Prefix,
+                  }));
+                }}
+              >
+                <div title={folder.Prefix}>
+                  <Image
+                    src="/folder.png"
+                    alt="Folder Icon"
+                    width={150}
+                    height={150}
+                  />
+                  <div className="text-center">
+                    {folder.Prefix.slice(0, -1).length > 18
+                      ? `${folder.Prefix.slice(0, -1).substring(0, 18)}...`
+                      : folder.Prefix.slice(0, -1)}
+                  </div>
                 </div>
               </div>
-            </div>
-          ))}
-        {objects?.length > 0 &&
-          objects.map((object: any) => (
-            <div
-              key={object.Name} // key should be on the outermost element
-              className="flex flex-col items-center justify-center p-4 bg-gray-800 rounded-lg hover:bg-gray-700 transition"
-            >
-              <div title={object.Key}>
-                <Image src="/pdf.png" alt="pdf icon" width={150} height={150} />
-                <div className="text-center">
-                  {object.Key.length > 18
-                    ? `${object.Key.substring(0, 18)}...`
-                    : object.Key}
+            ))}
+          {objects?.length > 0 &&
+            objects.map((object: any) => (
+              <div
+                key={object.Name} // key should be on the outermost element
+                className="flex flex-col items-center justify-center p-4 bg-gray-800 rounded-lg hover:bg-gray-700 transition"
+              >
+                <div title={object.Key}>
+                  <Image
+                    src="/pdf.png"
+                    alt="pdf icon"
+                    width={150}
+                    height={150}
+                  />
+                  <div className="text-center">
+                    {object.Key.length > 18
+                      ? `${object.Key.substring(0, 18)}...`
+                      : object.Key}
+                  </div>
                 </div>
               </div>
-            </div>
-          ))}
-      </div>
-      {objects?.length === 0 && folders?.length === 0 && (
+            ))}
+        </div>
+      )}
+      {!loading && objects?.length === 0 && folders?.length === 0 && (
         <div>No objects found</div>
       )}
     </>
